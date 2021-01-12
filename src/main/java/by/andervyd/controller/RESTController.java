3ecf71f2289a28423097407fe325d890a8c0c66d
andervyd <andervyd@gmail.com>
2021-01-12 21:08:17 +0200

    added update employee [@PutMapping]

--- src/main/java/by/andervyd/controller/RESTController.java
+++ src/main/java/by/andervyd/controller/RESTController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,14 @@ public class RESTController {
 
     @PostMapping("/employees")
     public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.savingEmployeeData(employee);
+
+        return  employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
 
         employeeService.savingEmployeeData(employee);
 
