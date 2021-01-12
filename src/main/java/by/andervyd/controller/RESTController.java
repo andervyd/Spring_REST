c7fd970aabba25903518d6e3f7c1ba087fcba173
andervyd <andervyd@gmail.com>
2021-01-12 17:40:27 +0200

    request for one employee {id}

--- src/main/java/by/andervyd/controller/RESTController.java
+++ src/main/java/by/andervyd/controller/RESTController.java
@@ -4,6 +4,7 @@ import by.andervyd.entity.Employee;
 import by.andervyd.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +25,11 @@ public class RESTController {
         return allEmployee;
     }
 
+    @GetMapping("employees/{id}")
+    public Employee getEmployee(@PathVariable Long id) {
 
+        Employee employee = employeeService.getEmployee(id);
 
-
-
+        return  employee;
+    }
 }
